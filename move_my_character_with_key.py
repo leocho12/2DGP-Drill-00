837bc569c1e03f439bc1553dffa851ac9babe7c2
leocho12 <hjc021212@gmail.com>
2025-09-23 13:07:34 +0900

    ESC key active

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -16,6 +16,13 @@ x,y=TUK_WIDTH//2,TUK_HEIGHT//2
 def key_events():
     global running,dir
 
+    events=get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key==SDLK_ESCAPE:
+            running = False
+
     pass
 
 
