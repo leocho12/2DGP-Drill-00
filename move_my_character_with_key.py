c5280b5e4e83a5602c03e1a2fb2402626499f793
leocho12 <hjc021212@gmail.com>
2025-09-23 12:51:51 +0900

    실습6 골격 제작

---
+++ move_my_character_with_key.py
@@ -0,0 +1,25 @@
+from pico2d import *
+
+
+TUK_WIDTH,TUK_HEIGHT = 1280,1024
+open_canvas(TUK_WIDTH,TUK_HEIGHT)
+tuk_ground=load_image('TUK_GROUND.png')
+character = load_image('animation_sheet.png')
+
+running = True
+frame = 0
+
+x,y=TUK_WIDTH//2,TUK_HEIGHT//2
+
+while running:
+    clear_canvas()
+
+    tuk_ground.draw(TUK_WIDTH//2,TUK_HEIGHT//2)
+    character.clip_draw(frame*100,100*1,100,100,x,y)
+    key_events()
+    update_canvas()
+    frame = (frame + 1) % 8
+    delay(0.05)
+
+
+close_canvas()
